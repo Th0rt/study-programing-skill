d86f6853232a32b1129bbab7b5adbe68d1286041
Th0rt <yamaxanado@gmail.com>
2019-10-06 17:07:54 +0900

    devide func and test

--- chapter1.py
+++ chapter1.py
@@ -30,24 +30,3 @@ def q_3(s: str, l: int, prefix: str = "") -> bool:
         c = s[0]
 
     return q_3(s=s[1:], l=l-1, prefix=prefix + c)
-
-
-def test_q_1():
-    assert q_1("abcd")
-    assert not q_1("aaaa")
-
-
-def test_q_2():
-    assert q_2("abcde", "edcba")
-    assert not q_2("abcde", "edcbe")
-
-
-def test_q_3():
-    assert q_3("Mr John Smith ", 13) == "Mr%20John%20Smith"
-    assert not q_3("MrJohnSmith", 11) == "Mr%20John%20Smith"
-
-
-if __name__ == "__main__":
-    test_q_1()
-    test_q_2()
-    test_q_3()
---
+++ test_chapter1.py
@@ -0,0 +1,20 @@
+import unittest
+import chapter1
+
+
+class TestChapter1(unittest.TestCase):
+    def test_q_1(self):
+        assert chapter1.q_1("abcd")
+        assert not chapter1.q_1("aaaa")
+
+    def test_q_2(self):
+        assert chapter1.q_2("abcde", "edcba")
+        assert not chapter1.q_2("abcde", "edcbe")
+
+    def test_q_3(self):
+        assert chapter1.q_3("Mr John Smith ", 13) == "Mr%20John%20Smith"
+        assert not chapter1.q_3("MrJohnSmith", 11) == "Mr%20John%20Smith"
+
+
+if __name__ == "__main__":
+    unittest.main()
