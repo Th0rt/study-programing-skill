acd10d2fa87f21bf97fe9733b9cc7f09fe29cf6f
Th0rt <yamaxanado@gmail.com>
2019-11-04 20:04:38 +0900

    init (#9)

--- chapter2/linked_list.py
+++ chapter2/linked_list.py
@@ -1,6 +1,12 @@
 import unittest
 
 
+class LinkedListNode:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
     def __init__(self, *args):
         self.head, self.tail = self.create_nodes(values=args)
@@ -8,13 +14,14 @@ class LinkedList:
 
     def next(self):
         self.current = self.current.next
+        return self.current.value
 
-    def get_node(self):
+    def get_node(self) -> LinkedListNode:
         return self.current
 
     def create_nodes(self, head=None, tail=None, values=[]):
         if not values:
-            return head, tail
+            return (head, tail) if tail else (head, head)
 
         elif not head:
             return self.create_nodes(head=LinkedListNode(values[0]), tail=None, values=values[1:])
@@ -27,8 +34,7 @@ class LinkedList:
             tail.next = LinkedListNode(values[0])
             return self.create_nodes(head=head, tail=tail.next, values=values[1:])
 
-
-class LinkedListNode:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
+    def append_node_to_tail(self, node: LinkedListNode):
+        self.tail.next = node
+        self.tail = node
+        return self
