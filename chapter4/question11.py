dd060b4465de3d9b4c91dca2655a7dcb29d00648
Tsukasa Yamada <yamada.tsukasa@panair.jp>
2020-02-02 14:19:47 +0900

    change filename

--- chapter4/question11.py
+++ chapter4/question11.py
@@ -1,45 +1,40 @@
-def is_patial_tree(small_tree, big_tree):
-
-    def _compare(entire, partial):
-        if not partial:
-            return True
-
-        if not entire:
-            return False
-
-        if not entire.value == partial.value:
-            return False
-
-        if partial.left:
-            return _compare(entire.left, partial.left)
-
-        if partial.right:
-            return _compare(entire.right, partial.right)
-
-        return True
-
-    big_tree_root = search_value(big_tree, small_tree.root.value)
-    if not big_tree_root:
-        return False
-    else:
-        return _compare(big_tree_root, small_tree.root)
+from random import randint
 
 
+class BinaryTreeNode:
+    def __init__(self, id, value, left=None, right=None):
+        self.id = id
+        self.value = value
+        self.left = left
+        self.right = right
 
-def search_value(tree, value):
-    def _search(node1, value):
-        if node1.value == value:
-            return node1
 
-        if node1.left:
-            left_search = _search(node1.left, value)
-            if left_search:
-                return left_search
-        if node1.right:
-            right_search = _search(node1.right, value)
-            if right_search:
-                return right_search
+class BinaryTree:
+    def __init__(self, values):
+        self.root = self._create_nodes(values)
+        self.node_count = len(values)
+
+    def _create_nodes(self, values, ids=None):
+        if not values:
+            return None
+        if not ids:
+            ids = list(range(len(values)))
+
+        mid_index = len(values) // 2
+        node = BinaryTreeNode(id=ids[mid_index], value=values[mid_index])
+        node.left = self._create_nodes(values[:mid_index], ids=ids[:mid_index])
+        node.right = self._create_nodes(
+            values[mid_index + 1 :], ids=ids[mid_index + 1 :]
+        )
+        return node
+
+    def get_random_node(self):
+        i = randint(0, self.node_count)
+        node = self.root
+
+        while node:
+            if node.id == i:
+                return node
+            node = node.left if node.id > i else node.right
 
         return None
-
-    return _search(tree.root, value)
