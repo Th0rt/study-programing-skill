69009599faa3b70a4b24e780312b0958d080effe
Tsukasa Yamada <yamada.tsukasa@panair.jp>
2020-04-12 13:01:39 +0900

    stdoutできることまで

---
+++ chapter6/question3.py
@@ -0,0 +1,17 @@
+import unittest
+
+class ChaseBord:
+    rows = []
+    def __init__(self, *args, **kwargs):
+        self.
+
+class Row:
+    length = 8
+    pass
+
+class Domino:
+    length = 2
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
--- chapter7/question3.py
+++ chapter7/question3.py
@@ -1,9 +1,30 @@
 import unittest
-from test.support import captured_stdout
+from typing import List
+import sys
+import io
+from contextlib import redirect_stdout
+
+
+class Wallet:
+
+    def __init__(self, credit: int = 0):
+        self.creadit = credit
+
+    def pay(self, value):
+        if value > self.creadit:
+            return ValueError()
+        else:
+            self.creadit -= value
+            return value
 
 
 class Person:
-    pass
+    def __init__(self, wallet: Wallet):
+        self.wallet = wallet
+
+    def pay(self, target):
+        print(target)
+        return target.receipt(self.wallet.pay(target.credit_value))
 
 
 class Cusomter(Person):
@@ -14,15 +35,7 @@ class JukeBoxOwner(Person):
     pass
 
 
-class Wallet:
-    pass
-
-
-class Coin:
-    pass
-
-
-class JukeBox:
+class Song:
     pass
 
 
@@ -30,24 +43,33 @@ class Record:
     pass
 
 
-class Song:
-    pass
+class JukeBox:
+    credit_value: int = 1
+
+    def __init__(self, owner: JukeBoxOwner, records: List[Record] = []):
+        self.owner = owner
+        self.records = records
+        self.gain = 0
+
+    def receipt(self, credit_value):
+        self.gain += self.credit_value
+        print("select song!")
 
 
 class TestJukeBox(unittest.TestCase):
     def setUp(self):
-        self.owner = JukeBoxOwner()
-        self.jukebox = JukeBox(owner=owner)
-        self.customer = Cusomter()
+        self.owner = JukeBoxOwner(wallet=Wallet())
+        self.jukebox = JukeBox(owner=self.owner)
+        self.customer = Cusomter(wallet=Wallet(credit=2))
+        self.stdout = io.StringIO()
 
     def test_it(self):
 
-        with captured_stdout as stdout:
-            self.customer.insert_coin(self.jukebox)
-            stdout_lines = stdout.getvalue.splitlines()
+        with redirect_stdout(self.stdout):
+            self.customer.pay(self.jukebox)
 
-            assert stdout_lines[-2] == "Please Select Song!"
-            assert stdout_lines[-1] == "1) 幽雅に咲かせ、墨染の桜 ～Border of Life"
+        stdout = self.stdout.getvalue().splitlines()
+        assert stdout[1] == "select song!"
 
 
 if __name__ == "__main__":
